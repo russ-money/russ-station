38d55b9e8fe4719935979d03544b88fc79cfc58c
skeeyui <19658265+skeyuui@users.noreply.github.com>
2024-11-04 23:52:59 -0300

    update paths

--- tools/UpdatePaths/lazy.bat
+++ tools/UpdatePaths/lazy.bat
@@ -1,6 +1,6 @@
 @echo off
-set "start_date=2024/10/21"
-set "end_date=2024/10/28"
+set "start_date=2024/10/27"
+set "end_date=2024/11/04"
 for /f "delims=" %%A in ('powershell -Command "Get-ChildItem -Path .\Scripts\*.txt | Where-Object { $_.LastWriteTime -ge '%start_date%' -and $_.LastWriteTime -le '%end_date%' } | Sort-Object LastWriteTime | ForEach-Object { $_.FullName }"') do (
     call "%~dp0\..\bootstrap\python" -m UpdatePaths "%%A"
 )
